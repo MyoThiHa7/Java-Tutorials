5c5d2121465649178556e674076557bc52af3e63
Myo ThiHa <thihaz2b@gmail.com>
2024-08-16 22:55:32 +0630

    16-8-2024 10:55PM

---
+++ StudyRightNow/src/Learning/Test4.java
@@ -0,0 +1,19 @@
+package Learning;
+
+public class Test4 {
+	public static void main(String args[])
+	{
+		byte num1=10,num2=5;
+		System.out.println("Number1 = "+num1);
+		System.out.println("Number2 = "+num2);
+		System.out.println("Number1 - Number2 = "+(num1-num2));
+		System.out.println(num1+"-"+num2+"="+(num1-num2));
+		System.out.println("Number1 * Number2 = "+(num1*num2));
+		System.out.println(num1+"*"+num2+"="+(num1*num2));
+		System.out.println("Number1 / Number2 = "+(num1/num2));
+		System.out.println(num1+"/"+num2+"="+(num1/num2));
+		System.out.println("Number 1 %Number2 ="+(num1%num2));
+		System.out.println(num1+"%"+num2+"="+(num1%num2));
+	}
+	
+}
